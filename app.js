8bf77a23a0f1fa9e3f650ec2fbda0fec634cf40a
gganj123 <zwol10@naver.com>
2024-10-24 23:00:51 +0900

    study : 회원가입 기능 추가 postman 유저저장 성공

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const indexRouter = require("./routes/index");
 const app = express();
 
 require("dotenv").config();
 app.use(cors());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use("/api", indexRouter);
+// api/user
 const mongoURI = process.env.LOCAL_DB_ADDRESS;
 mongoose
-  .connect(mongoURI, { useNewUrlParser: true })
+  .connect(mongoURI)
   .then(() => console.log("mongoose connected"))
   .catch((err) => console.log("DB connection fail, err"));
 
