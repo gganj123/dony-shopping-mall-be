9901f29cb67ab8fc0d271c7ec6a87b6d551ab632
gganj123 <zwol10@naver.com>
2024-10-24 22:01:04 +0900

    study :start shopping-mall Schema : Cart, Order, Product, User

---
+++ Model/Order.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const User = require("./User");
+const Product = require("./Product");
+const Schema = mongoose.Schema;
+const orderSchema = Schema(
+  {
+    orderNum: { type: String },
+    shipTo: { type: String, required: true },
+    contact: { type: String, required: true },
+    totalprice: { type: Number, default: 0, required: true },
+    userId: { type: mongoose.ObjectId, ref: User, required: true },
+    status: { type: String, default: "preparing" },
+    items: [
+      {
+        productId: { type: mongoose.ObjectId, ref: Product, required: true },
+        size: { type: String, required: true },
+        qty: { type: Number, default: 1, required: true },
+        price: { type: Number, required: true },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+userSchema.method.toJSON = function () {
+  const obj = this._doc;
+  delete obj.updateAt;
+  return obj;
+};
+
+const Order = mongoose.model("Order", orderSchema);
+module.exports = Order;
---
+++ Model/Product.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const productSchema = Schema(
+  {
+    sku: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    image: { type: String, required: true },
+    category: { type: Array, required: true },
+    discription: { type: String, required: true },
+    price: { type: Number, required: true },
+    stock: { type: Object, required: true },
+    status: { type: String, default: "active" },
+    isDeleted: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+userSchema.method.toJSON = function () {
+  const obj = this._doc;
+  delete obj.updateAt;
+  return obj;
+};
+
+const Product = mongoose.model("product", productSchema);
+module.exports = Product;
