b5ba856cd9724794df46c7b6c52095709e4e2ebb
gganj123 <zwol10@naver.com>
2024-10-30 19:23:31 +0900

    study : getProduct 에 page기능 추가

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,4 +1,5 @@
 const Product = require("../models/Product");
+const PAGE_SIZE = 5;
 const productController = {};
 
 productController.createProduct = async (req, res) => {
@@ -38,9 +39,17 @@ productController.getProducts = async (req, res) => {
     const { page, name } = req.query;
     const cond = name ? { name: { $regex: name, $options: "i" } } : {};
     let query = Product.find(cond);
+    let response = { status: "success" };
+    if (page) {
+      query.skip((page - 1) * PAGE_SIZE).limit(PAGE_SIZE);
+      const totalItemNum = await Product.find(cond).count();
+      const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+      response.totalPageNum = totalPageNum;
+    }
 
     const productList = await query.exec();
-    res.status(200).json({ status: "success", data: productList });
+    response.data = productList;
+    res.status(200).json(response);
   } catch (error) {
     res.status(400).json({ status: "fail", error: error.message });
   }
